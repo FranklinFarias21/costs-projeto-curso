05afb72c0add489b46b6e6a9548d43a731f5a75f
FranklinFarias21 <franklinfarias9982@gmail.com>
2023-04-28 09:50:12 -0500

    Salvando dados do formulario de contato

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,33 @@
 import ContactForm from '../project/ContactForm';
+import {useNavigate} from 'react-router-dom'
 import "./Contact.css"
 
-function ContactPage() {
+
+const ContactPage = () => {
+
+    const history = useNavigate()
+
+    const createPost = (project) => {
+        fetch("http://localhost:5000/contato", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify(project),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            history('/')
+        })
+        .catch((err) => console.log(err))
+    }
+
     return (
         <div className='contact'>
             <h1>Contato</h1>
             <p>Caso possua alguma dúvida, entre em contato!</p>
-            <ContactForm />
+            <ContactForm handleSubmit={createPost} btn_text="Enviar mensagem"/>
 
         </div>
     )
--- src/components/project/ContactForm.js
+++ src/components/project/ContactForm.js
@@ -1,18 +1,31 @@
 import Input from "../form/Input"
 import TextArea from "../form/TextArea"
 import SubmitButton from "../form/SubmitButton"
+import { useState } from "react"
 import "./ContactForm.css"
 
-const ContactForm = () => {
+const ContactForm = ({handleSubmit, btn_text, projectData}) => {
+
+    const [project, setProject] = useState(projectData || {})
+
+    const submit = (e) => {
+        e.preventDefault()
+        handleSubmit(project)
+    }
+
+    const handleChange = (e) => {
+        setProject({ ...project, [e.target.name]: e.target.value })
+    }
+
     return (   
-        <form className="form">
-            <Input type="text" text="Nome" name="nome" placeholder="Insira o nome" />
-            <Input type="email" text="E-mail" name="email" placeholder="Insira o e-mail" />
-            <Input type="text" text="Assunto" name="assunto" placeholder="Informe o assunto"  />
-            <TextArea text="Mensagem" name="mensagem" placeholder="Insira a mensagem"  />
-            <SubmitButton text="Enviar mensagem" />
+        <form onSubmit={submit} className="form">
+            <Input type="text" text="Nome" name="nome" placeholder="Insira o nome" handleOnChange={handleChange} value={project.nome ? project.nome : ''} />
+            <Input type="text" text="E-mail" name="email" placeholder="Insira o e-mail" handleOnChange={handleChange} value={project.email ? project.email : ''}/>
+            <Input type="text" text="Assunto" name="assunto" placeholder="Informe o assunto"  handleOnChange={handleChange} value={project.assunto ? project.assunto : ''}/>
+            <TextArea text="Mensagem" name="mensagem" placeholder="Insira a mensagem"  handleOnChange={handleChange} value={project.mensagem ? project.mensagem : ''}/>
+            <SubmitButton text={btn_text} />
         </form>
-    );
+    )
 }
 
-export default ContactForm;
+export default ContactForm
